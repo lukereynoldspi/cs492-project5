68bb77b3c7f8e0bc8ca29243bb3400b517b36fac
Luke Reynolds <lukereynoldspi@gmail.com>
2023-03-14 15:47:22 -0700

    added dummy folders and refactored files

---
+++ wasteagram/lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wasteagram',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wasteagram'),
+      ),
+      body: Center(
+        child: Text(
+          'Math sucks ass',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
