5d0d5540e93595f5bd7386f89fdf6525bb2f2b98
lukereynoldspi <lukereynoldspi@gmail.com>
2023-03-18 17:24:58 -0700

    configured flutterfire and firebase

--- wasteagram/lib/main.dart
+++ wasteagram/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import './app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize any plugins or services here
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
